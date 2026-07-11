e20977274e531526cfe5316636ab6b4eefe2a369
shivamdubey023 <dubeysk2003@gmail.com>
2026-07-11 19:15:53 +0530

    Auto-Sync: Find The Index Of The First Occurrence In A String -

---
+++ submissions/find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -0,0 +1,12 @@
+# Problem: Find The Index Of The First Occurrence In A String
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        n,m = len(haystack), len(needle)
+
+        for i in range (n-m +1):
+            if haystack [i:i+m] == needle:
+                return i
+        return -1
